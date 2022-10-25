222b3463cb8082ebf08cec181899f7641c412314
yevhensd <yevhen.sd@gmail.com>
2022-10-25 23:00:39 +0300

    View login page

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,8 @@ class MyApp extends StatelessWidget {
       ),
       home: SplashScreen(
         seconds: 3,
-        navigateAfterSeconds: const MyHomePage(title: 'Flutter Demo Home Page'),
+        navigateAfterSeconds: const MyFormPage(
+            title: 'Form'), //const MyHomePage(title: 'Flutter Demo Home Page'),
         title: const Text(
           'SplashScreen',
           style: TextStyle(
@@ -29,6 +30,92 @@ class MyApp extends StatelessWidget {
         ),
         backgroundColor: Colors.lightBlue[200],
       ),
+    );
+  }
+}
+
+class MyFormPage extends StatefulWidget {
+  const MyFormPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyFormPage> createState() => _MyFormPageState();
+}
+
+class _MyFormPageState extends State<MyFormPage> {
+  final _sizeTextBlack = const TextStyle(fontSize: 20.0, color: Colors.black);
+  final _sizeTextWhite = const TextStyle(fontSize: 20.0, color: Colors.white);
+  bool _rememberMe = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Form'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            width: 400.0,
+            child: TextFormField(
+              decoration: const InputDecoration(labelText: "Login"),
+              keyboardType: TextInputType.emailAddress,
+              style: _sizeTextBlack,
+            ),
+          ),
+          Container(
+            width: 400.0,
+            padding: const EdgeInsets.only(top: 10.0),
+            child: TextFormField(
+              decoration: const InputDecoration(labelText: "Password"),
+              obscureText: true,
+              style: _sizeTextBlack,
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+            child: Row(
+              children: <Widget>[
+                Theme(
+                  data: ThemeData(
+                      unselectedWidgetColor:
+                          const Color.fromARGB(255, 197, 182, 94)),
+                  child: Checkbox(
+                    value: _rememberMe,
+                    onChanged: (value) {
+                      setState(() {
+                        _rememberMe = value!;
+                      });
+                    },
+                  ),
+                ),
+                const Text(
+                  'Запам\'ятати',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 101, 94, 199)),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 25.0),
+            child: MaterialButton(
+              color: Theme.of(context).colorScheme.secondary,
+              height: 50.0,
+              minWidth: 150.0,
+              onPressed: () {},
+              child: Text(
+                "Далі",
+                style: _sizeTextWhite,
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
