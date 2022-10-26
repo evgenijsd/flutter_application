75844cf898038834905279799b5eaa5fa0d48bf9
yevhensd <yevhen.sd@gmail.com>
2022-10-26 19:54:39 +0300

    Added loginization check and automatic input

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: import_of_legacy_library_into_null_safe
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
@@ -21,8 +24,7 @@ class MyApp extends StatelessWidget {
       ),
       home: SplashScreen(
         seconds: 3,
-        navigateAfterSeconds: const MyFormPage(
-            title: 'Form'), //const MyHomePage(title: 'Flutter Demo Home Page'),
+        navigateAfterSeconds: const MyFormPage(title: 'Form'),
         title: const Text(
           'SplashScreen',
           style: TextStyle(
@@ -33,6 +35,8 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+getdata() async {}
 
 class MyFormPage extends StatefulWidget {
   const MyFormPage({super.key, required this.title});
@@ -46,7 +50,45 @@ class MyFormPage extends StatefulWidget {
 class _MyFormPageState extends State<MyFormPage> {
   final _sizeTextBlack = const TextStyle(fontSize: 20.0, color: Colors.black);
   final _sizeTextWhite = const TextStyle(fontSize: 20.0, color: Colors.white);
+  final _errorText = const TextStyle(color: Colors.red);
   bool _rememberMe = false;
+  static const loginKey = 'login';
+  static const passwordKey = 'password';
+  static const rememberKey = 'remember';
+  String _login = '';
+  String _password = '';
+  String _error = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _initLogin();
+  }
+
+  Future _initLogin() async {
+    await _getLogin();
+  }
+
+  Future _setLogin() async {
+    var prefs = await SharedPreferences.getInstance();
+    prefs.setString(loginKey, _login);
+    prefs.setString(passwordKey, _password);
+    prefs.setBool(rememberKey, _rememberMe);
+  }
+
+  Future _getLogin() async {
+    var prefs = await SharedPreferences.getInstance();
+    _login = prefs.getString(loginKey) ?? '';
+    _password = prefs.getString(passwordKey) ?? '';
+    _rememberMe = prefs.getBool(rememberKey) ?? false;
+    if (_login == "admin" && _password == "123456" && _rememberMe) {
+      // ignore: use_build_context_synchronously
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const MyHomePage(title: 'Second Page')));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,21 +100,34 @@ class _MyFormPageState extends State<MyFormPage> {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          SizedBox(
-            width: 400.0,
-            child: TextFormField(
-              decoration: const InputDecoration(labelText: "Login"),
-              keyboardType: TextInputType.emailAddress,
-              style: _sizeTextBlack,
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: SizedBox(
+              width: 400.0,
+              child: TextFormField(
+                decoration: const InputDecoration(labelText: "Login"),
+                keyboardType: TextInputType.emailAddress,
+                style: _sizeTextBlack,
+                onChanged: (text) {
+                  setState(() {
+                    _login = text;
+                  });
+                },
+              ),
             ),
           ),
           Container(
             width: 400.0,
-            padding: const EdgeInsets.only(top: 10.0),
+            padding: const EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
             child: TextFormField(
               decoration: const InputDecoration(labelText: "Password"),
               obscureText: true,
               style: _sizeTextBlack,
+              onChanged: (text) {
+                setState(() {
+                  _password = text;
+                });
+              },
             ),
           ),
           SizedBox(
@@ -107,13 +162,39 @@ class _MyFormPageState extends State<MyFormPage> {
               color: Theme.of(context).colorScheme.secondary,
               height: 50.0,
               minWidth: 150.0,
-              onPressed: () {},
+              onPressed: () {
+                if (_login.compareTo("admin") == 0 &&
+                    _password.compareTo("123456") == 0) {
+                  setState(() {
+                    _error = '';
+                  });
+                  _setLogin();
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              const MyHomePage(title: 'Second Page')));
+                } else {
+                  setState(() {
+                    _error = 'Невірний логін';
+                  });
+                }
+              },
               child: Text(
                 "Далі",
                 style: _sizeTextWhite,
               ),
             ),
-          )
+          ),
+          Center(
+            child: SizedBox(
+              height: 50.0,
+              child: Text(
+                _error,
+                style: _errorText,
+              ),
+            ),
+          ),
         ],
       )),
     );
@@ -130,14 +211,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -148,23 +221,12 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Hello World',
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
+              'Вітаю',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
